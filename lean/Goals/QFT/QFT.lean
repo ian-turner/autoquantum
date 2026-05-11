6584f80f84c13ddde333302ec37cf29e94b5ce8e
ian-turner <i.r.turner55@gmail.com>
2026-05-11 14:13:16 -0400

    remove comment from qft goal

--- lean/Goals/QFT/QFT.lean
+++ lean/Goals/QFT/QFT.lean
@@ -4,7 +4,6 @@ open AutoQuantum AutoQuantum.QFT Matrix
 
 noncomputable def qftCircuit (n : ℕ) : Circuit n := sorry
 
-/-- The QFT circuit matrix equals the normalized DFT matrix. -/
 theorem qft_correct (n : ℕ) :
     (circuitMatrix (qftCircuit n)) = qftMatrix n := by
   sorry
